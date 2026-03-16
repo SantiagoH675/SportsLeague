eed3d71ae3a66953920e8d96a1a97c3d462c677e
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2026-03-15 22:12:21 -0500

    Ready 1

--- SportsLeague.Domain/Entities/AuditBase.cs
+++ SportsLeague.Domain/Entities/AuditBase.cs
@@ -2,6 +2,7 @@
 {
     public abstract class AuditBase
     {
+        public int Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; set; }
     }
